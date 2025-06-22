56fc8f6b18b49e0f55ffc921003f16269ebdad6b
Lucas <lucaschaves1370dev@gmail.com>
2025-06-21 22:09:48 -0300

    script e index inciais feitos

---
+++ public/script.js
@@ -0,0 +1,24 @@
+//Function to load the links
+async function loadLinks()
+{
+    //"Ask" the backend for the links
+    const container = document.getElementById('links-container');
+    const response = await fetch ('/api/links');
+    const links = await response.json();
+
+    //Clear any example contents
+    container.innerHTML = '';
+
+    //Create a button for each link
+    links.array.forEach(link => {
+        const button = document.createElement('a');
+        button.href = link.url;
+        button.target = '_blank'; //Open in a new tab
+        button.className = 'link-button';
+        button.innerText = link.title;
+        container.appendChild(button);
+    });
+}
+
+//load the links
+loadLinks();
